753e485c5ebb1797f34d8530b9786459f9551103
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-02 00:13:08 +0000

    how to delete files and check if they exist

--- modules.js
+++ modules.js
@@ -18,3 +18,30 @@ const fileSystem = require("fs");
 // fileSystem.writeFile("./random.txt", inf, () => {
 //   console.log("file written");
 // });
+
+// check if files exist
+// if (!fileSystem.existsSync("./temp")) {
+//   fileSystem.mkdir("./temp", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder created");
+//   });
+// } else {
+//   fileSystem.rmdir("./temp", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder removed");
+//   });
+// }
+// deleting files
+// if (fileSystem.existsSync("./docs/deleteme.txt")) {
+//   fileSystem.unlink("./docs/deleteme.txt", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("file removed");
+//   });
+// }
+// 32.50 net ninja node tutorial 2
