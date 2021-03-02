dc3a9fd9defd92fc8e7b76c228ed55be710666d8
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-02 22:09:48 +0000

    have learned how to rout with node. will start express tomorrow, need to do a new branch.

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   // set header content type.
   res.setHeader("Content-Type", "text/html");
-  // set data to be sent back
-  res.write("<p>hello this is the data</p>");
-  res.write("<p>hello this is also the data</p>");
-  // instruct for response to go back.
-  res.end();
+  // set an html file
+
+  // the path as the files are in the views folder
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+
+    default:
+      path += "/404.html";
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(data);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
   console.log("listening for requests on port 3000");
 });
+//Node.js Crash Course Tutorial #5 - NPM
